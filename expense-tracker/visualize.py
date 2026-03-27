25ca33b74892cf8b8c873aa4453a516aa3f60665
mowmonira123 <monira.mow@tu-dortmund.de>
2026-03-27 22:51:37 +0100

    Added plot expenses

--- expense-tracker/visualize.py
+++ expense-tracker/visualize.py
@@ -1,2 +1,10 @@
 import matplotlib.pyplot as plt
 from .storage import get_summary
+
+
+def plot_expenses():
+    total, categories = get_summary()
+
+    if not categories:
+        print("No data to display.")
+        return
