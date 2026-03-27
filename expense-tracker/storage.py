3b00dd197eddfd98d25d90bd26c7298fc447d498
unknown <you@example.com>
2026-03-27 22:00:15 +0100

    Add expense

--- expense-tracker/storage.py
+++ expense-tracker/storage.py
@@ -2,3 +2,7 @@ import os
 
 FILE_NAME = "expenses.txt"
 LIMIT_FILE = "limit.txt"
+
+def add_expense(amount, category):
+    with open(FILE_NAME, "a") as f:
+        f.write(f"{amount},{category}\n")
