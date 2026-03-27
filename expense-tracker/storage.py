a0a387de16ce00b0d145313b0dd32abc9a72bf19
unknown <you@example.com>
2026-03-27 22:02:10 +0100

    Add get_expenses function

--- expense-tracker/storage.py
+++ expense-tracker/storage.py
@@ -6,3 +6,18 @@ LIMIT_FILE = "limit.txt"
 def add_expense(amount, category):
     with open(FILE_NAME, "a") as f:
         f.write(f"{amount},{category}\n")
+        
+        
+def get_expenses():
+    if not os.path.exists(FILE_NAME):
+        return []
+
+    expenses = []
+    with open(FILE_NAME, "r") as f:
+        for line in f:
+            amount, category = line.strip().split(",")
+            expenses.append({
+                "amount": float(amount),
+                "category": category
+            })
+    return expenses
