9ef8722fceed35b93ca2e04bc09a1910c7b156a2
mowmonira123 <monira.mow@tu-dortmund.de>
2026-03-27 22:18:01 +0100

    Detect if terminal supports colors

--- expense-tracker/tracker.py
+++ expense-tracker/tracker.py
@@ -9,3 +9,17 @@ from .storage import (
 )
 from .visualize import plot_expenses
 import os
+
+
+# Detect if terminal supports colors
+USE_COLORS = os.name != "nt" or "ANSICON" in os.environ
+
+
+class Colors:
+    HEADER = "\033[95m" if USE_COLORS else ""
+    BLUE = "\033[94m" if USE_COLORS else ""
+    CYAN = "\033[96m" if USE_COLORS else ""
+    GREEN = "\033[92m" if USE_COLORS else ""
+    WARNING = "\033[93m" if USE_COLORS else ""
+    RED = "\033[91m" if USE_COLORS else ""
+    END = "\033[0m" if USE_COLORS else ""
